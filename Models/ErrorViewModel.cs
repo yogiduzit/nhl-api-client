f7443e932f5d70b4b40d265a7e91362a2a0fcb7b
yogiduzit <yverma2@my.bcit.ca>
2021-02-17 15:39:12 -0800

    Change project type to use local db

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace comp4870_assignment_1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
