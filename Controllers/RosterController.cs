455036ffc6d54997f694a481c7578ddd6d956035
yogiduzit <yverma2@my.bcit.ca>
2021-02-20 11:40:13 -0800

    Minor fixes and final touches

--- Controllers/RosterController.cs
+++ Controllers/RosterController.cs
@@ -4,28 +4,16 @@ using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 using comp4870_assignment_1.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 
 namespace comp4870_assignment_1.Controllers
 {
+    [Authorize]
     public class RosterController : Controller
     {
         const string BASE_URL = "https://statsapi.web.nhl.com/api/v1";
-        public async Task<IActionResult> Index()
-        {
-            List<RosterItem> roster = new List<RosterItem>();
-            using (var httpClient = new HttpClient())
-            {
-                using (var response = await httpClient.GetAsync($"{BASE_URL}/teams/2/roster"))
-                {
-                    string apiResponse = await response.Content.ReadAsStringAsync();
-                    roster = JsonConvert.DeserializeObject<List<RosterItem>>(apiResponse);
-                }
-            }
-            return View(roster);
-        }
-
         public async Task<IActionResult> PlayerAsync(string link)
         {
             PlayerList playerList = new PlayerList();
