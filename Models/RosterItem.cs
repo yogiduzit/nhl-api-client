d936709fdef0fa0734c7d7b4966fbcf2420a713b
yogiduzit <yverma2@my.bcit.ca>
2021-02-20 17:00:14 -0800

    Styling + Cleanup

--- Models/RosterItem.cs
+++ Models/RosterItem.cs
@@ -1,8 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace comp4870_assignment_1.Models
 {
     public class RosterItem
     {
         public Person person { get; set; }
+
+        [Display(Name="Jersey Number")]
         public string jerseyNumber { get; set; }
         public Position position { get; set; }
     }
